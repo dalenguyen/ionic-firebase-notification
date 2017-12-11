926aff5e4bde9a8cccd796cd536782c5df7970da
Dale Nguyen <dungnq@itbox4vn.com>
2017-12-11 17:19:17 -0500

    Added push logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {Push, PushObject, PushOptions} from '@ionic-native/push';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,13 +11,45 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public alertCtrl: AlertController, public push: Push) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
+      this.pushsetup();
       splashScreen.hide();
     });
   }
-}
 
+  pushsetup(){
+    const options: PushOptions = {
+      android: {
+        senderID: 'here your SENDER ID from FCM'
+      },
+      ios: {
+        alert: 'true',
+        badge: true,
+        sound: 'false'
+      },
+      windows: {}
+    }
+
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('notification').subscribe((notification: any) => {
+      if(notification.additionalData.foreground){
+        let youralert = this.alertCtrl.create({
+          title: 'New Push notification',
+          message: notification.message
+        });
+        youralert.present();
+      }
+    });
+
+    pushObject.on('registration').subscribe((registration: any) => {
+     //do whatever you want with the registration ID
+    });
+
+    pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
+    }
+}
