f438d4f7916057d451eed08c9a72ad4180f35260
Dale Nguyen <dungnq@itbox4vn.com>
2017-12-12 15:02:01 -0500

    Finalized push configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
+
 import { Component } from '@angular/core';
 import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {Push, PushObject, PushOptions} from '@ionic-native/push';
-
 import { HomePage } from '../pages/home/home';
+import { Push, PushObject, PushOptions} from '@ionic-native/push';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public alertCtrl: AlertController, public push: Push) {
+  constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public alertCtrl:AlertController, public push: Push) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -21,35 +21,36 @@ export class MyApp {
     });
   }
 
-  pushsetup(){
+  pushsetup() {
     const options: PushOptions = {
-      android: {
-        senderID: 'here your SENDER ID from FCM'
-      },
-      ios: {
-        alert: 'true',
-        badge: true,
-        sound: 'false'
-      },
-      windows: {}
+     android: {
+         senderID: '918519375898'
+     },
+     ios: {
+         alert: 'true',
+         badge: true,
+         sound: 'false'
+     },
+     windows: {}
+  };
+
+  const pushObject: PushObject = this.push.init(options);
+
+  pushObject.on('notification').subscribe((notification: any) => {
+    if (notification.additionalData.foreground) {
+      let youralert = this.alertCtrl.create({
+        title: 'New Push notification',
+        message: notification.message
+      });
+      youralert.present();
     }
+  });
 
-    const pushObject: PushObject = this.push.init(options);
-
-    pushObject.on('notification').subscribe((notification: any) => {
-      if(notification.additionalData.foreground){
-        let youralert = this.alertCtrl.create({
-          title: 'New Push notification',
-          message: notification.message
-        });
-        youralert.present();
-      }
-    });
-
-    pushObject.on('registration').subscribe((registration: any) => {
+  pushObject.on('registration').subscribe((registration: any) => {
      //do whatever you want with the registration ID
-    });
+     alert(registration);
+  });
 
-    pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
-    }
+  pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
+  }
 }
